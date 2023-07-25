75cf4e10142a6ec5cdc465734a83ef1a13e5c742
Daniel Gerendasy <danielgerendasy@gmail.com>
2023-07-24 23:10:25 -0700

    Updated horizon discord links

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -107,7 +107,7 @@ const Home = () => {
 
                 <PageCard>
                     <HomeCardContent
-                        link={"https://discord.gg/5rTWNKHy"}
+                        link={"https://discord.com/invite/horizonps"}
                         icon={"https://assets-global.website-files.com/6257adef93867e50d84d30e2/62595384e89d1d54d704ece7_3437c10597c1526c3dbd98c737c2bcae.svg"}
                         iconAltText="Discord Logo"
                         title={"Horizon Discord"}
